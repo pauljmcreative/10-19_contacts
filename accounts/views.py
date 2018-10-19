cf0d10baa418a92fa06d26c16270b5e5725f32fd
Paul Mourraille <paul@pauljmphoto.com>
2018-10-19 14:43:00 -0700

    adds user reg method and login required decorator

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,38 @@ from django.shortcuts import render, redirect
 # IMPORT DJANGO AUTH
 from django.contrib import auth
 
+# Import django user model
+from django.contrib.auth.models import User
+
+def register(request):
+  if request.method == 'POST':
+    # get form values
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    username = request.POST['username']
+    email = request.POST['email']
+    password = request.POST['password']
+    password2 = request.POST['password2']
+
+    # Check to see if passwords match
+    if password == password2:
+      # Check if username exists
+      if User.objects.filter(username=username).exists():
+        return render(request, 'accounts/register.html', {'error': 'That username is already registered.  Please try different username.'})
+      else:
+        # check for email
+        if User.objects.filter(email=email).exists():
+          return render(request, 'accounts/register.html', {'error': 'That email has already been registered'})
+        else:
+          # Register User
+          user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+          user.save()
+          return redirect('login')
+    else:
+      return render(request, 'accounts/register.html', {'error': 'Passwords do not match'})
+  else:
+    return render(request, 'accounts/register.html')
+
 def login(request):
   if request.method == 'POST':
     username = request.POST['username']
--- contacts/views.py
+++ contacts/views.py
@@ -3,10 +3,15 @@ from django.shortcuts import render
 #  IMPORT CONTACT MODEL
 from .models import Contact
 
+# IMPORT LOGIN REQUIRED DECORATOR
+from django.contrib.auth.decorators import login_required
+
+@login_required
 def contacts_index(request):
   contacts = Contact.objects.all()
   return render(request, 'contacts/contacts_index.html', {'contacts': contacts})
 
+@login_required
 def contacts_show(request, contact_id):
   contact = Contact.objects.get(id=contact_id)
   return render(request, 'contacts/contacts_show.html', {'contact': contact})
